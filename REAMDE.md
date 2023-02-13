6411f4ae11bd9d0ada9becfb9a64b67b31517f4a
abenteuerzeit <amroz.muzyka@gmail.com>
2023-02-13 11:27:34 +0100

    set up express

---
+++ REAMDE.md
@@ -0,0 +1,8 @@
+# Basic app template
+
+1. nodemon to watch for changes and restart the server
+2. Babel to enable JavaScript feature not enabled in Node.js
+3. Dotenv to load environment variables from a .env file
+4. Express for backend
+5. Middleware
+    1. CORS to enable cross-origin requests
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('JS Project');
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
